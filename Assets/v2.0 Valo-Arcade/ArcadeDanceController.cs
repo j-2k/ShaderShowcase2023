7e4c96af52db2f530bda718ae8672d586ed7fef7
Juma <juma_ad@hotmail.com>
2023-05-16 14:34:46 +0400

    Good progress on arrow gen

--- Assets/v2.0 Valo-Arcade/ArcadeDanceController.cs
+++ Assets/v2.0 Valo-Arcade/ArcadeDanceController.cs
@@ -34,51 +34,56 @@ public class ArcadeDanceController : MonoBehaviour
 
 
 
-    Vector3 GetRandomDirection(float angle, bool isRandom = false)
+    Vector3 GetRandomDirection(float angle, bool isRandom = true)
     {
         if(isRandom)
         {
             float randomRadian = Random.Range(0, Mathf.PI*2);
-            Debug.Log(randomRadian + "RRAND RADIAN1");
             Vector3 direction = new Vector3(Mathf.Sin(randomRadian), 0, Mathf.Cos(randomRadian));
             if(Vector3.Angle(dir, direction) <= 90)
             {
-                Debug.Log("Angle is < 90, Gen New");
                 direction.x *= -1;
                 direction.z *= -1;
-                Debug.Log(randomRadian + "RRAND RADIAN2");
             }
-
-            //Debug.Log(direction);
-            return direction;
-
+            return Vector3.Normalize(direction);
         }
         else
         {
             Vector3 direction = new Vector3(Mathf.Sin(Mathf.Deg2Rad * angle), 0, Mathf.Cos(Mathf.Deg2Rad * angle));
             Debug.Log(direction);
-            return direction;
+            return Vector3.Normalize(direction);
         }
     }
 
-    void RotARay()
+    float nextTimeToCheck;
+    [SerializeField] float secondsToGenNewRay = 5;
+    [SerializeField] float directionMagnitude = 5;
+    void DebugRotARay(bool isGenerating = false)
     {
-        if (Time.time >= a)
+        if(!isGenerating)
         {
-            dir = GetRandomDirection(angleTheta, true) * 3;
-            a = Time.time + 2;
+
+        }
+        else
+        {
+            if (Time.time >= nextTimeToCheck)
+            {
+                dir = GetRandomDirection(angleTheta, true) * directionMagnitude;
+                nextTimeToCheck = Time.time + secondsToGenNewRay;
+            }
         }
         Debug.DrawRay(transformPos, dir, Color.green);
+
     }
-    float a;
     // Update is called once per frame
     void Update()
     {
-        RotARay();
+        DebugRotARay();
 
         deformShaderMat.SetFloat("_Strength", Mathf.PingPong(Time.time*2,1));//(-0.5f,0.5f, Mathf.Abs(Mathf.Sin(Time.time))));
         transformPos = transform.position + Vector3.up * 1.0f;
         EnumDanceStates();
+       
     }
 
     void EnumDanceStates()
@@ -92,18 +97,22 @@ public class ArcadeDanceController : MonoBehaviour
 
             case DanceStages.Generate:
                 Debug.Log("Generate");
+                dir = GetRandomDirection(angleTheta, true) * directionMagnitude;
                 if (MainDanceArrow == null)
                 {
-                    MainDanceArrow = GameObject.Instantiate(danceArrowFab, GenRandom8GridPos(transform),
-                        Quaternion.LookRotation(transform.position));
+                    MainDanceArrow = GameObject.Instantiate(danceArrowFab, transformPos + dir,
+                        Quaternion.LookRotation(transformPos - (transformPos + dir)));
                 }
                 else
                 {
-                    MainDanceArrow.transform.position = GenRandom8GridPos(transform);
+                    MainDanceArrow.transform.position = transformPos + dir;
+                    MainDanceArrow.transform.rotation = Quaternion.LookRotation(transformPos - (transformPos + dir));
                     MainDanceArrow.SetActive(true);
                 }
                 currentEnum = DanceStages.Playing;
                 break;
+
+
             case DanceStages.Playing:
                 Debug.Log("Playing");
                 speed += 7 * Time.deltaTime;
@@ -115,6 +124,8 @@ public class ArcadeDanceController : MonoBehaviour
                     speed = 1;
                 }
                 break;
+
+
             case DanceStages.Ending:
                 Debug.Log("Ending");
                 break;
@@ -132,5 +143,12 @@ public class ArcadeDanceController : MonoBehaviour
     {
         //Ditching this idea Random Direction Vec will look better
         return Vector3.zero + Vector3.up * 1f;
+    }
+
+    private void OnDrawGizmos()
+    {
+        //Vector3 GizmoDir = GetRandomDirection(0);
+        //Gizmos.DrawRay(transformPos, GizmoDir);
+        //Gizmos.DrawSphere(transformPos + GizmoDir, 0.1f);
     }
 }
