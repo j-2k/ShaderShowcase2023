327b99e4e5dbaa31dad9d8f3e17831c1e65d3ea3
Juma <juma_ad@hotmail.com>
2023-05-24 11:34:29 +0400

    Trying to implement accel

--- Assets/v2.1 Quake Sim/QuakeMovement.cs
+++ Assets/v2.1 Quake Sim/QuakeMovement.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class QuakeMovement : MonoBehaviour
 {
+    [Header("SKIP")]
     [SerializeField] Vector3 wishDir;
     CharacterController cc;
 
     [SerializeField] float rotX, rotY, sens, angle;
 
     Camera cam;
+
+    [Header("MAIN PARAMS")]
+    [SerializeField] float maxSpeed;
+    [SerializeField] float maxAccel;
+    [SerializeField] Vector3 vel;
+
+    [SerializeField] float currentSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -25,7 +33,8 @@ public class QuakeMovement : MonoBehaviour
         MouseHandler();
 
         VectorManagement();
-        QuakeAcceleration();
+
+        QuakeMove();
 
         DebugMovementVectors();
     }
@@ -33,20 +42,46 @@ public class QuakeMovement : MonoBehaviour
     Vector3 localForward;
     Vector3 localRight;
 
+    
+
     void VectorManagement()
     {
         localForward = new Vector3(Mathf.Sin(rotX * Mathf.Deg2Rad), 0, Mathf.Cos(rotX * Mathf.Deg2Rad));
         localRight = new Vector3(Mathf.Cos(rotX * Mathf.Deg2Rad), 0, -Mathf.Sin(rotX * Mathf.Deg2Rad));
 
-        wishDir = (Input.GetAxisRaw("Horizontal") * localRight + Input.GetAxisRaw("Vertical") * localForward).normalized;
+        wishDir = (Input.GetAxis("Horizontal") * localRight + Input.GetAxis("Vertical") * localForward);
+        if(wishDir.magnitude > 1)
+        {
+            wishDir.Normalize();
+        }
     }
 
-    void QuakeAcceleration()
+    void QuakeMove()
     {
+        /*
+        if(cc.isGrounded) //cc is grounded is trash
+        { UpdateGroundedVelocity(); }
+        else
+        { UpdateAirVelocity(); }
+        */
+
+        UpdateGroundedVelocity();
+    }
 
 
+    void UpdateGroundedVelocity()
+    {
+        Debug.Log("Grounded!");
 
-        cc.Move(wishDir * 5 * Time.deltaTime);
+        vel += wishDir * maxSpeed;
+
+        cc.Move(vel * Time.deltaTime);
+    }
+
+    void UpdateAirVelocity()
+    {
+        Debug.LogWarning("IN AIR!!!");
+        //cc.Move(wishDir * 5 * Time.deltaTime);
     }
 
     void DebugMovementVectors()
@@ -75,4 +110,23 @@ public class QuakeMovement : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawSphere(transform.position + debugCCVelocity + Vector3.up, 0.25f);
     }
+
+    /* PASTED FROM ID SOFTWARE QUAKE / WINQUAKE / SV_USER.C LINE 190
+    void SV_Accelerate (void)
+    {
+	    int			i;
+	    float		addspeed, accelspeed, currentspeed;
+
+	    currentspeed = DotProduct (velocity, wishdir);
+	    addspeed = wishspeed - currentspeed;
+	    if (addspeed <= 0)
+	    	return;
+	    accelspeed = sv_accelerate.value*host_frametime*wishspeed;
+	    if (accelspeed > addspeed)
+	    	accelspeed = addspeed;
+	    
+	    for (i=0 ; i<3 ; i++)
+	    	velocity[i] += accelspeed*wishdir[i];	
+    }
+    */
 }
