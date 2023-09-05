b680fb9067a2e0d8bdad2e066e90f96ee4498b99
Juma <juma_ad@hotmail.com>
2023-09-05 12:39:03 +0400

    shelf ranger done

--- Assets/v2.1 Quake Sim/scripts/TrailRelatedGarbo/AutoTrailEffect.cs
+++ Assets/v2.1 Quake Sim/scripts/TrailRelatedGarbo/AutoTrailEffect.cs
@@ -13,7 +13,6 @@ public class AutoTrailEffect : MonoBehaviour
     [SerializeField] SkinnedMeshRenderer smrREF;
     public static SkinnedMeshRenderer smr;
     public static float[] smrIndices = new float[6];
-    [SerializeField] bool spawnAfterglow = false;
 
     float t = 0;
     // Start is called before the first frame update
@@ -24,12 +23,15 @@ public class AutoTrailEffect : MonoBehaviour
         {
             Debug.LogWarning("SKINNED MESH RENDERER IS MISSING!");
         }
+        rend = target.GetComponent<Renderer>();
     }
+    Renderer rend;
+    
 
     // Update is called once per frame
     void Update()
     {
-        if(spawnAfterglow)//200 * 0.1 //200/10
+        if(rend.isVisible)//200 * 0.1 //200/10
         {
             t += Time.deltaTime;
             for (int i = 0; i < 6; i++)//canbeoptimized
@@ -39,7 +41,8 @@ public class AutoTrailEffect : MonoBehaviour
             if(t > timeSpawnRateMS) //0.034 = 3 enabled @ one MAX//canbeoptimized
             {
                 //Instantiate is really bad but idc for now. Subsititue for ObjectPool or just an enabling algo.
-                Instantiate(trailingRanger,transform.position - target.transform.forward, target.transform.rotation,target.transform);
+                GameObject ranger = Instantiate(trailingRanger, target.transform.position - target.transform.forward*0.2f, target.transform.rotation,target.transform);
+                ranger.transform.parent = null;
                 //GameObject.Destroy(currentTrail, 0.1f);
                 t = 0;
             }
